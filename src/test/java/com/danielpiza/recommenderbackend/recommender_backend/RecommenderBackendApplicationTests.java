1deb7aaa119558d98ebb7d134c9d8328088e4472
Daniel Piza <danielpiza0816@gmail.com>
2025-01-18 11:24:56 -0500

    Initial commit

---
+++ src/test/java/com/danielpiza/recommenderbackend/recommender_backend/RecommenderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielpiza.recommenderbackend.recommender_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
